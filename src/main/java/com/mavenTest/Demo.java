eabc5f2ef08e544c931f3e88a9b95443b2846d4d
azhou7980 <2017786680@qq.com>
2023-04-25 10:43:28 +0800

    Initial commit

---
+++ src/main/java/com/mavenTest/Demo.java
@@ -0,0 +1,7 @@
+package com.mavenTest;
+
+public class Demo {
+    public void testOutput(){
+        System.out.println("这是maven创建后第一句话！");
+    }
+}
---
+++ src/test/java/com/mavenTest/DemoTest.java
@@ -0,0 +1,11 @@
+package com.mavenTest;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void TestDemo(){
+        new Demo().testOutput();
+    }
+}
